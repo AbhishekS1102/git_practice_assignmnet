49cd829c143fbeb1f46850b84bfc22c61ed9b15e
abhishek sharma <abksharma23@gmail.com>
2022-09-13 18:39:14 +0530

    Added Prime and Palindrome problem

---
+++ Prime_Problem.js
@@ -0,0 +1,13 @@
+let num=14;
+let count=0;
+for(let i=1;i<=num;i++){
+    if(num%i==1){
+        count+=1;
+    }
+
+}
+if(count<=2){
+    console.log("Yes it is a Prime number");
+}else{
+    console.log("No it not a Prime Number");
+}
---
+++ palindrome_problem.js
@@ -0,0 +1,11 @@
+let str="abcba"
+let bag="";
+   for(let i=str.length;i>0;i--){
+       bag=bag+str[i];
+   }
+   
+   if(bag==str){
+       console.log("Yes it ia Palindrome");
+   }else{
+       console.log("No it is not a Palindrone");
+   }
